cb7d804c8d473a61e5159978d8d981afc78c8f7d
Rishikesh <42427672+rishiks199@users.noreply.github.com>
2024-02-03 03:17:44 +0530

    complete code

---
+++ README.md
@@ -0,0 +1,28 @@
+# Groupy---A-chat-application-for-your-peers-
+Connect and chat with multiple persons in a single time with private room. 
+
+
+
+
+Requirement in your system
+ 1 - node
+ 2 - nodemon
+ 3 - momoent.js
+ 4 - socket.io
+ 5 - express
+ 
+ if you don't have these in your system then open cmd and type -> npm install express moment socket.io
+ it will install in one go
+ ------------------------------------------------------------End of Requirement ----------------------------
+step - 1
+
+fork the Repo in your github profile and clone the Repository in your local computer
+
+step - 2
+
+Be sure to install node in your system
+then in cmd [carefull with the working directory] type nodemon server.js
+and in the url bar in just type [ localhost:300 ] 
+
+
+
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const path = require('path')
+const socketio= require('socket.io');
+const http = require('http');
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser,userLeave,getRoomUsers} = require('./utils/users');
+
+
+const app = express();
+
+const server = http.createServer(app)
+const io = socketio(server);
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const botName  = 'Bot is here ';
+
+//Run when Client connects
+io.on('connection',socket =>{
+    socket.on('joinRoom',({username,room})=>{
+       
+        const user = userJoin(socket.id,username,room);
+       // console.log(user);
+        socket.join(user[user.length-1].room);
+        //welcomes the new user
+        socket.emit('message',formatMessage(botName,'Welcome to the Chat'));
+
+      //  console.log('New WebSocket Js');
+        //socket.emit -> sends or emits message to a single user
+        //socket.broadcast.emit ->sends a message to every one but not you
+        //io.emit -> sends the message to every one including you in general
+        //console.log(user[0].username);
+        socket.broadcast
+        .to(user[user.length-1].room)
+        .emit('message',formatMessage(botName,`${user[user.length-1].username} has Joined the Chat`));
+        
+
+        //sends users and room info so that DOM can use and place the names and rooms in left side 
+
+io.to(user[user.length-1].room).emit('roomUsers',{
+    room:user[user.length-1].room,
+    users:getRoomUsers(user[user.length-1].room)  
+     
+    });
+ });
+ 
+       
+//listen to chat Message
+socket.on('user-message',msg=>{
+
+    const user = getCurrentUser(socket.id);
+  //  console.log(user);
+
+    io.to(user.room)
+    .emit('message',formatMessage(user.username,msg));
+})
+
+//Runs when client Disconnects
+
+socket.on('disconnect',()=>{
+    const user = userLeave(socket.id);
+
+    if(user){
+        io.to(user.room)
+        .emit('message',
+        formatMessage(botName,`${user.username} has left the Chat`)
+        );
+
+        io.to(user.room).emit('roomUsers',{
+            room:user.room,
+            users:getRoomUsers(user.room)  
+             
+            });
+    }
+
+    
+})
+
+});
+
+const PORT = 300 || process.env.PORT;
+//server
+server.listen(PORT,()=>{
+    console.log(`Server is running on Port ${PORT}`);
+});
+
